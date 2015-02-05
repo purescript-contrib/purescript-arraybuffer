8cbc425df2d453565ed08fb96dfa22e98c1e4770
Jorge Acereda <jacereda@gmail.com>
2015-02-06 00:06:27 +0100

    Added Advancer.purs

---
+++ src/Data/ArrayBuffer/Advancer.purs
@@ -0,0 +1,20 @@
+module Data.ArrayBuffer.Advancer where
+
+import Data.ArrayBuffer.DataView(DataView())
+import Data.ArrayBuffer.Types
+import Control.Monad.Eff
+
+type Advancer = { dv :: DataView, off :: ByteOffset }
+
+foreign import advance
+"""
+function advance(s) {
+  return function(d) {
+    return function() {
+      var off = d.off;
+      d.off += s;
+      return off;
+    };
+  };
+}
+""" :: forall e. ByteOffset -> Advancer -> Eff ( |e) ByteOffset
