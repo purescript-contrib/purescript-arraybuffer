fa8d1b6f931124e38ba4f3a2d216f4abcfa47991
Athan Clark <athan.clark@gmail.com>
2018-12-12 08:16:34 -0700

    removed funky show module

--- src/Data/ArrayBuffer/Show.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-// module Show
-
-exports.showViaInspect = function showViaInspect (a) {
-  return require('util').inspect(a);
-}
--- src/Data/ArrayBuffer/Show.purs
+++
@@ -1,5 +0,0 @@
-module Data.ArrayBuffer.Show where
-
-import Data.ArrayBuffer.Types (ArrayView)
---
-foreign import showViaInspect :: forall a. ArrayView a -> String
