970351c810e387add70fd74e83f437b409bce563
Jorge Acereda <jacereda@gmail.com>
2019-01-17 13:21:14 +0100

    Sort includes

--- test/Main.purs
+++ test/Main.purs
@@ -1,13 +1,10 @@
 module Test.Main where
 
-import Test.Properties (propertiesTests)
-
 import Prelude
+
 import Effect (Effect)
 import Effect.Console (log)
-
-
-
+import Test.Properties (propertiesTests)
 
 main :: Effect Unit
 main = do
