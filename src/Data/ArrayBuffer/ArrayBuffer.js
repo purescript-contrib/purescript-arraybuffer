e3d384ccca811b9421ad83cc8c9aef9e3f2a4eef
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-02-21 18:31:18 -0600

    Ran lebab

--- src/Data/ArrayBuffer/ArrayBuffer.js
+++ src/Data/ArrayBuffer/ArrayBuffer.js
@@ -6,10 +6,10 @@ exports.emptyImpl = function empty (s) {
     return new ArrayBuffer(s);
 };
 
-exports.byteLength = function byteLength (a) {
+export function byteLength(a) {
     return a.byteLength;
-};
+}
 
-exports.sliceImpl = function sliceImpl (a, s, e) {
+export function sliceImpl(a, s, e) {
     return a.slice(s, e);
-};
+}
